258a8e46f841bdb1d36a9c05c2cdb039545852d1
Daniel Shamaeli <d_sh80@yahoo.com>
2017-10-20 12:05:11 +0100

    Ad comment to Magic Square

--- MagicSquare.java
+++ MagicSquare.java
@@ -1,21 +1,38 @@
+/** Author:Daniel Shamaeli, Student ID:201230464
+    this is the third assignment for COMP517
+    https://github.com/dshamaeli/magicSquares.git
+*/
+//Class for containing MagicSquare Data
+class MagicSquare {
+    private int int1; //The first magic key 
+    private int int2; //the second magic key
+    private int int3; //The third magic key
 
+    //class constructor colour 
+    MagicSquare(int int1, int int2, int int3) {
+        this.int1 = int1; // this is the current object, this allows us to use the same name for variables 
+        this.int2 = int2; 
+        this.int3 = int3;
+    }       
 
-class MagicSquare
-{
-  private int int1;
-  private int int2;
-  private int int3;
-  MagicSquare(int int1, int int2,int int3)
-  {
-    this.int1 = int1;
-    this.int2 = int2;
-    this.int3 = int3;
-  }
-  
-  public void print()
-  { 
-    System.out.println((int3 - int2) + "\t" + (int3 + (int1 + int2)) + "\t" + (int3 - int1) + "\n");
-    System.out.println(int3 - (int1 - int2) + "\t" + int3 + "\t" + (int3 + (int1 - int2)) + "\n");
-    System.out.println((int3 + int1) + "\t" + (int3 - (int1 + int2)) + "\t" + (int3 + int2 + "\n"));
-  }  
+    /*
+    Method to print Magic Square:
+    
+        column1         column2         column3  
+        c – b          c + (a + b)       c – a
+       c – (a – b)          c           c + (a – b)
+        c + a          c – (a + b)       c + b
+    */
+
+    public void conjureUp() {
+
+        //first row
+        System.out.println((int3 - int2) + "\t" + (int3 + (int1 + int2)) + "\t" + (int3 - int1) + "\n");
+
+        //second row    
+        System.out.println(int3 - (int1 - int2) + "\t" + int3 + "\t" + (int3 + (int1 - int2)) + "\n");
+
+        //third row
+        System.out.println((int3 + int1) + "\t" + (int3 - (int1 + int2)) + "\t" + (int3 + int2 + "\n"));
+    }
 }
--- SquareMaster.java
+++ SquareMaster.java
@@ -1,38 +1,43 @@
+
+/** Author:Daniel Shamaeli, Student ID:201230464
+    this is the third assigment for COMP517
+    https://github.com/dshamaeli/magicSquares.git
+*/
 import java.util.Scanner;
 
-class SquareMaster{
-  private static int firstNum, secondNum, thirdNum;
-  private static Scanner input = new Scanner(System.in);
+class SquareMaster {
+    private static int firstNum, secondNum, thirdNum;
+    private static Scanner input = new Scanner(System.in);
 
-  public static void sort(int num1, int num2, int num3) {
-    firstNum = Math.min(num1, Math.min(num2, num3));
-    thirdNum = Math.max(num1, Math.max(num2, num3));
-    secondNum = num1 + num2 + num3 - firstNum - thirdNum;
-  }
-
-  public static void main(String[] args) {
-    System.out.print("Please enter your three Magic Keys(one or more 0 to exit): ");
-    do {
-      firstNum = input.nextInt();
-      secondNum = input.nextInt();
-      thirdNum = input.nextInt();
-      sort(firstNum, secondNum, thirdNum);
-      System.out.println("\nThis is the SquareMaster!\n");
-      System.out.println("You have given me " + firstNum + " " + secondNum + " " + thirdNum + "\n");
-      if (0 < firstNum && firstNum < secondNum && secondNum < (thirdNum - firstNum) && 
-                                                            (secondNum != 2 * firstNum))
-        break;
-      if (firstNum <= 0 || secondNum <=0 || thirdNum <= 0)
-        break; 
-      System.out.println("these are not valid keys!!!");
-      System.out.print("\nPlease enter your three Magic Keys again(one or more 0 to exit): ");
-    } while (firstNum > 0 && secondNum > 0 && thirdNum > 0 );
-    if (firstNum <= 0 || secondNum <=0 || thirdNum <= 0){
-      System.out.println("YOU MAY NOT ENTER!!!");
-      return; 
+    public static void sort(int num1, int num2, int num3) {
+        firstNum = Math.min(num1, Math.min(num2, num3));
+        thirdNum = Math.max(num1, Math.max(num2, num3));
+        secondNum = num1 + num2 + num3 - firstNum - thirdNum;
     }
-    System.out.println("The Keys are appropriate. Below is your Magic Square. Use it wisely!\n");
-    MagicSquare square = new MagicSquare(firstNum, secondNum, thirdNum);
-    square.print();
-  }
+
+    public static void main(String[] args) {
+        System.out.print("Please enter your three Magic Keys(one or more 0 to exit): ");
+        do {
+            firstNum = input.nextInt();
+            secondNum = input.nextInt();
+            thirdNum = input.nextInt();
+            sort(firstNum, secondNum, thirdNum);
+            System.out.println("\nThis is the SquareMaster!\n");
+            System.out.println("You have given me " + firstNum + " " + secondNum + " " + thirdNum + "\n");
+            if (0 < firstNum && firstNum < secondNum && secondNum < (thirdNum - firstNum)
+                    && (secondNum != 2 * firstNum))
+                break;
+            if (firstNum <= 0 || secondNum <= 0 || thirdNum <= 0)
+                break;
+            System.out.println("these are not valid keys!!!");
+            System.out.print("\nPlease enter your three Magic Keys again(one or more 0 to exit): ");
+        } while (firstNum > 0 && secondNum > 0 && thirdNum > 0);
+        if (firstNum <= 0 || secondNum <= 0 || thirdNum <= 0) {
+            System.out.println("YOU MAY NOT ENTER!!!");
+            return;
+        }
+        System.out.println("The Keys are appropriate. Below is your Magic Square. Use it wisely!\n");
+        MagicSquare square = new MagicSquare(firstNum, secondNum, thirdNum);
+        square.conjureUp();
+    }
 }
